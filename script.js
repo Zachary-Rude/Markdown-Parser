84c444bbf65f4562efa1ef30f0d6200be22803e7
Zachary-Rude <zacharyrude10@gmail.com>
2025-09-04 17:28:34 -0700

    Re-added code copy button

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-hljs.addPlugin(new CopyButtonPlugin());
-
 const { Marked } = window.marked;
 const { markedHighlight } = window.markedHighlight;
 
@@ -19,4 +17,23 @@ const marked = new Marked(
 
 markdownInput.addEventListener("input", function() {
     markdownOutput.innerHTML = marked.parse(markdownInput.value);
+
+    // code taken and modified from https://www.eddymens.com/blog/adding-a-copy-button-to-highlightjs-code-snippets
+    var snippets = document.getElementsByTagName("pre");
+    var numberOfSnippets = snippets.length;
+
+    for (var i = 0; i < numberOfSnippets; i++) {
+        code = snippets[i].getElementsByTagName("code")[0].innerText;
+        snippets[i].classList.add("hljs"); // append copy button to pre tag
+        snippets[i].innerHTML = "<button class=\"hljs-copy-button\">Copy</button>" + snippets[i].innerHTML; // append copy button
+        snippets[i].getElementsByClassName('hljs-copy-button')[0].addEventListener("click", function () {
+            this.innerText = 'Copying..';
+            navigator.clipboard.writeText(code);
+            this.dataset.copied = true;
+            button = this;
+            setTimeout(function () {
+                button.dataset.copied = false;
+            }, 1000)
+        });
+    }
 }, false);
